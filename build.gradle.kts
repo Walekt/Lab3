c0f51e88a5bdb72de6bfb80fe1bf03b586382a2f
Sashok <alehandror2005@gmail.com>
2025-03-29 21:46:50 +0700

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,18 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("org.projectlombok:lombok:1.18.30")
+    annotationProcessor ("org.projectlombok:lombok:1.18.30")
+    testImplementation ("org.projectlombok:lombok:1.18.30")
+    testAnnotationProcessor ("org.projectlombok:lombok:1.18.30")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    implementation ("com.google.code.gson:gson:2.10.1")
 }
+
+
+
 
 tasks.test {
     useJUnitPlatform()
 }
-]
